9bf28739eb399f3651f19bc9de1ee675e639e052
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-11-25 01:38:54 -0300

    solution: sharing success

--- android-trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -1,13 +1,22 @@
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ShareCompat
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
@@ -26,16 +35,40 @@ class GameWonFragment : Fragment() {
         Toast.makeText(context,
             "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
             Toast.LENGTH_LONG).show()
-        // TODO (01) Add setHasOptionsMenu(true)
-        // This allows onCreateOptionsMenu to be called
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.winner_menu, menu)
+                // check if the activity resolves
+                if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+                    // hide the menu item if it doesn't resolve
+                    menu.findItem(R.id.share).isVisible = false
+                }
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.share -> shareSuccess()
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
     }
 
-    // TODO (02) Create getShareIntent method
-    // TODO (03) Create shareSuccess method
-    // TODO (04) Override and fill out onCreateOptionsMenu
-    // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
-    // resolve
-    // TODO (05) Override onOptionsItemSelected
-    // Call the shareSuccess method when the item id matches R.id.share
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            type = "text/plain"
+        }
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 }
