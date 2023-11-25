c3f000d359d8e41e26e953ddb01892ac0227c712
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-11-24 23:17:35 -0300

    solution: back stack manipulation

--- android-trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -14,8 +15,9 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_over, container, false)
-        // TODO (05) Add an onClick Handler for the tryAgain button that navigates to the gameFragment
-        // using action_gameOverFragment_to_gameFragment
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
