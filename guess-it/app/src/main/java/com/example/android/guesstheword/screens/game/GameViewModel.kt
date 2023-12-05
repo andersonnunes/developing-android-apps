0fdf312d42eb055e5f086caf98a33f08c89b48fe
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-12-05 18:24:50 -0300

    exercise: create the GameViewModel

---
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.android.guesstheword.screens.game
+
+// TODO (02) Create the GameViewModel class, extending ViewModel
+// TODO (03) Add init and override onCleared; Add log statements to both
