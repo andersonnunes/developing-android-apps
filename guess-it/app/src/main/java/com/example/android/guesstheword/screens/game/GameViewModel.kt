61f021f83f2d6c4b487f44fd9ed62b5fe343d68d
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-12-07 08:24:46 -0300

    exercise: add end game event

--- guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -22,6 +22,9 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    // TODO (01) Make a properly encapsulated LiveData called eventGameFinish that holds a
+    // boolean
+
     init {
         resetList()
         nextWord()
@@ -64,7 +67,8 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-           //  gameFinished()
+            //  gameFinished()
+            // TODO (03) Set eventGameFinish to true, to signify that the game is over
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -81,4 +85,7 @@ class GameViewModel : ViewModel() {
         _score.value = (score.value)?.plus(1)
         nextWord()
     }
+
+    // TODO (02) Make the function onGameFinishComplete which makes the value of eventGameFinish
+    // false
 }
